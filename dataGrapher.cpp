8982fc2e7cc1f24e20666b6c4f37fbbca594530e
Quinton <qemiller@vt.edu>
2019-02-06 16:39:06 -0500

    initial vagrantfile dataGrapher and dataPoint commit

---
+++ dataGrapher.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <cstdlib>
+#include ""
+
+class dataGrapher
+{
+public:
+	dataGrapher();
+	~dataGrapher();
+
+private:
+	new 
+};
+
+dataGrapher::dataGrapher()
+{
+}
+
+dataGrapher::~dataGrapher()
+{
+}
---
+++ dataPoint.cpp
@@ -0,0 +1,16 @@
+
+class dataPoint
+{
+public:
+	dataPoint();
+	~dataPoint();
+	dataPoint(double timeIn, double dataIn) { time = timeIn; data = dataIn; }
+	double setTime(double timeIn) { time = timeIn; }
+	double setData(double dataIn) { data = dataIn; }
+	double getTime() { return time; }
+	double getData() { return data; }
+
+private:
+	double time;
+	double data;
+};
