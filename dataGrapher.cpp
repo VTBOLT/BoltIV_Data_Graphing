1391596529bb5b36e8ca01aea3f550d952b6b293
cjosh-vt <cjosh@vt.edu>
2019-02-11 10:49:42 -0500

    part of a parser, have to push to checkout master

--- dataGrapher.cpp
+++ dataGrapher.cpp
@@ -1,21 +1,37 @@
 #include <vector>
+#include <string>
 #include <cstdlib>
-#include ""
+#include "dataPoint.hpp"
 
 class dataGrapher
 {
 public:
 	dataGrapher();
 	~dataGrapher();
+	std::vector<dataPoint> prettyParse(std::string infile);
+	std::vector<dataPoint> quickParse(std::string infile);
 
 private:
-	new 
+	std::vector<dataPoint> list;
 };
-
+int main(int argc, char** argv)
+{
+	dataGrapher();
+}
 dataGrapher::dataGrapher()
 {
+	
 }
 
 dataGrapher::~dataGrapher()
 {
+}
+
+std::vector<dataPoint> dataGrapher::prettyParse(std::string infile)
+{
+	
+}
+std::vector<dataPoint> dataGrapher::quickParse(std::string infile)
+{
+	
 }
