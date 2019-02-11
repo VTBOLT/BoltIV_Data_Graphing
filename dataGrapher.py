5955685529d00f6df027e93e8a8f205470206f75
cjosh-vt <cjosh@vt.edu>
2019-02-11 14:54:04 -0500

    added command line arguments for prettyPrint vs quickPrint. Pretty print works as of now, but quickPrint does nothing. Code does run syntax is: python dataGrapher.py -p data2.csv

--- dataGrapher.py
+++ dataGrapher.py
@@ -1,12 +1,32 @@
-import csv
+import csv, sys, getopt
 
-with open('data2.csv', mode='r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print('Column names are {0}'.format(", ".join(row)))
-            line_count += 1
-        print('\tTime:{0} Speed:{1}.'.format(row["Time"],row["Motor Speed"]))
-        line_count += 1
-    print('Processed {0} lines.'.format(line_count))
+def main(argv):
+	inputfile = 'data.csv'
+	try:
+		opts, args = getopt.getopt(argv,"hp:q:",["ifile="])
+	except getopt.GetoptError:
+		print 'dataGrapher.py -[option]\n-p <inputfile>\t--prettyPrint\n-q <inputStream>\t--quickPrint'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'dataGrapher.py -[option]\n-p <inputfile>\t--prettyPrint\n-q <inputStream>\t--quickPrint'
+			sys.exit()
+		elif opt in ("-p", "--prettyPrint"):
+			inputfile = arg
+			with open(inputfile, mode='r') as csv_file:
+				csv_reader = csv.DictReader(csv_file)
+				line_count = 0
+				for row in csv_reader:
+					if line_count == 0:
+						print('Column names are {0}'.format(", ".join(row)))
+						line_count += 1
+					print('\tTime:{0} Speed:{1}.'.format(row["Time"],row["Motor Speed"]))
+					line_count += 1
+				print('Processed {0} lines.'.format(line_count))
+		elif opt in ("-q", "--quickPrint"):
+			inputfile = arg
+			quickParse(inputfile)
+	print 'Input file is ', inputfile
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
