3b7573fa0c1b16dea30ce91201b25135197feb3a
cjosh-vt <cjosh@vt.edu>
2019-03-02 13:03:57 -0500

    testXbeeSend now sends test data instead of hello world

--- testXbeeSend.py
+++ testXbeeSend.py
@@ -1,6 +1,7 @@
+import csv
 from digi.xbee.devices import XBeeDevice
-PORT = "COM5"
-BAUD_RATE = 27600
+PORT = "COM6"
+BAUD_RATE = 57600
 
 
 def main():
@@ -12,12 +13,20 @@ def main():
 
     try:
         device.open()
-        device.send_data_broadcast("Hello XBee World!")
-
+        inputfile = 'data2.csv'
+        with open(inputfile, mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            firstLine=next(csv_reader)
+            head = [row for row in firstLine]
+            line_count = 1
+            for row in csv_reader:
+                line_count+=1
+                send = "({0},{1},{2},{3},{4},{5},{6},{7},{8},{9})".format(row[head[0]],row[head[1]],row[head[2]],row[head[3]],row[head[4]],row[head[5]],row[head[6]],row[head[7]],row[head[8]],row[head[9]])
+                device.send_data_broadcast(send)
     finally:
         if device is not None and device.is_open():
             device.close()
-
+            print ("Sent {} lines".format(line_count))
 
 if __name__ == '__main__':
     main()
